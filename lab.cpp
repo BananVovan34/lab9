51ac31803c94067455652f3d6407caa91e7c3cc3
BananVovan34 <ivavladimir777@gmail.com>
2024-12-26 18:32:07 +0300

    output of the sum of the entered numbers

--- lab.cpp
+++ lab.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
+#include <Windows.h>
 #include "std.h"
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	summaryOfInputNums();
+	return 0;
 }
--- std.h
+++ std.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <iostream>
 #ifndef STD_H
 #define STD_H
+
+void summaryOfInputNums() {
+	int n;
+	int result = 0;
+
+	while (true) {
+		std::cin >> n;
+		if (n == 0) break;
+		result += n;
+	}
+
+	std::cout << "Сумма введённых чисел равна " << result;
+}
 
 #endif
